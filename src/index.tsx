30f57db625a5fde3cacd3a70c4977d4f1cfc6e70
sartios <savramis.sartios@gmail.com>
2021-01-29 03:21:52 +0200

    chore: lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const theme = createMuiTheme({
+  palette: {}
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
